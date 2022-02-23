63b92519f3ee3e0066f44746404478a63847e030
checkmyprojects <notnow.plz+github@gmail.com>
2022-02-23 20:06:38 +0100

    delete item

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 function randomSelect() {
     const input = document.getElementById('textarea').value;
-    console.log(input);
+    // console.log(input);
     const lines = input.split('\n');
-    console.log(lines);
+    // console.log(lines);
     const randomLine = lines[Math.floor(Math.random() * lines.length)];
     
     //Display your line
     document.getElementById('winner').innerHTML = randomLine;
     // document.getElementById('textarea').style.display='none';
     // document.getElementById('resultado').style.display='block';
-    console.log(randomLine);
+    // console.log(randomLine);
     if (randomLine.length > 0){
         // creo una variable createLi y 
         let createLi = document.createElement("li");
@@ -17,4 +17,11 @@ function randomSelect() {
         createLi.appendChild(userInputText);
         document.getElementById("list").appendChild(createLi);
     }
+    let textAreaOld = document.getElementById('textarea').value;
+    console.log(textAreaOld);
+    console.log(randomLine);
+    // textAreaOld.value = textAreaOld.value.replace(randomLine.value, "");
+    document.getElementById('textarea').value = textAreaOld.replace(randomLine, "");
+    // document.getElementById('textarea').value = textAreaOld.replace('\n'+'\n', "");
+    console.log(textAreaOld);
 }
