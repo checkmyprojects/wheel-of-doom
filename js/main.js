7671cc5ce3be509cb20c448975e803112727a9a3
checkmyprojects <notnow.plz+github@gmail.com>
2022-02-23 15:32:50 +0100

    second commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+function randomSelect() {
+    const input = document.getElementById('textarea').value;
+    console.log(input);
+    const lines = input.split('\n');
+    console.log(lines);
+    const randomLine = lines[Math.floor(Math.random() * lines.length)];
+    
+    //Display your line
+    document.getElementById('winner').innerHTML = randomLine;
+    // document.getElementById('textarea').style.display='none';
+    // document.getElementById('resultado').style.display='block';
+    console.log(randomLine);
+    if (randomLine.length > 0){
+        // creo una variable createLi y 
+        let createLi = document.createElement("li");
+        let userInputText = document.createTextNode(randomLine);
+        createLi.appendChild(userInputText);
+        document.getElementById("list").appendChild(createLi);
+    }
+}
