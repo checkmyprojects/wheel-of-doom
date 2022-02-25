92f55a533445cd0883cc1c4272cd564f32bbd440
checkmyprojects <notnow.plz+github@gmail.com>
2022-02-25 08:34:17 +0100

    javascript clean

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,39 @@
 function randomSelect() {
+    let userInput = document.getElementById('textarea').value; // store textarea content in userInput
+    console.log(userInput);
+    let userInputSplit = userInput.split(/\r?\n/); // Split input text into an array of lines
+    console.log(userInputSplit);
+    userInputSplit = userInputSplit.filter(line => line.trim() !== ""); // filter out lines that are empty or contain only whitespace
+    console.log(userInputSplit);
+    userInput = userInputSplit.join("\n"); // join array into a string
+    document.getElementById('textarea').value = userInput; // replace textarea text with filtered text
+    const randomLine = userInputSplit[Math.floor(Math.random() * userInputSplit.length)]; // pick random line from filtered textarea array
+    document.getElementById('winner').innerHTML = randomLine;
+    if (randomLine.length > 0){
+        let createLi = document.createElement("li"); // create constructor createLi 
+        let userInputText = document.createTextNode(randomLine);
+        createLi.appendChild(userInputText);
+        document.getElementById("list").appendChild(createLi);
+    }
+    document.getElementById('textarea').value = userInput.replace(randomLine, "");
+}
+
+
+
+
+
+
+function randomSelect2() {
     // const input = document.getElementById('textarea').value;
     let input = document.getElementById('textarea').value;
     input = input + '\n'; 
-    textarea.value = input;
+    document.getElementById('textarea').value = input;
     // let trimmedText = textarea.value.replace(/\n+/g,'\n'); 
     // let trimmedText = input.replace(/\n+/g,'\n'); 
     // let trimmedText = input.replace(/^\s*$[\r\n]*|^[^\S\r\n]+|[^\S\r\n]+$|([^\S\r\n]){2,}/gm, '$1'); 
     let trimmedText = input.replace(/\n(\n+)/g, '$1'); 
     // let trimmedText = input.split('\n').filter(x=> x !== '').join('\n');
-    textarea.value = trimmedText;
+    document.getElementById('textarea').value = trimmedText;
     input = trimmedText.trim();
     // console.log(input);
     const lines = input.split('\n');
@@ -17,7 +42,7 @@ function randomSelect() {
     // const trimmedText = textarea.value.trim();
     // let trimmedText = textarea.value.replace(/\n+/g,'\n'); 
     // textarea.value = trimmedText;
-    console.log(trimmedText);
+    // console.log(trimmedText);
     //Display your line
     document.getElementById('winner').innerHTML = randomLine;
     // document.getElementById('textarea').style.display='none';
@@ -34,7 +59,8 @@ function randomSelect() {
     // console.log(textAreaOld);
     // console.log(randomLine);
     // textAreaOld.value = textAreaOld.value.replace(randomLine.value, "");
-    document.getElementById('textarea').value = textAreaOld.replace(randomLine + "\n", "");
+    // document.getElementById('textarea').value = textAreaOld.replace(randomLine + "\n", "");
+    document.getElementById('textarea').value = textAreaOld.replace(randomLine, "");
     // document.getElementById('textarea').value = textAreaOld.replace('\n'+'\n', "");
     // console.log(textAreaOld);
 }
