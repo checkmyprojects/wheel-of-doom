7ac6b4307543985aa441d791e08b691d836eba1d
Yessica Pérez León <yessicaperezleon@hotmail.com>
2022-02-28 12:39:25 +0100

    added dark mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,6 +58,12 @@ function countChars(obj){
     }
 }
 
+const btnSwitch = document.querySelector('#switch');
+
+btnSwitch.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    btnSwitch.classList.toggle('active');
+});
 
 
 
